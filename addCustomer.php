9d570f5660bf25bf746a27b02b2e15a3c61ebc42
Dilanya <96423@fhss.sjp.ac.lk>
2023-07-30 04:49:58 +0530

    create new customer

---
+++ addCustomer.php
@@ -0,0 +1,97 @@
+<?php
+
+require_once('config/db.php');
+
+$submissionStatus = "";
+
+$district_query = "SELECT id, district FROM `district`";
+$districtResult = mysqli_query($con, $district_query);
+
+if (isset($_POST['submit'])) {
+    $title = $_POST['title'];
+    $firstName = $_POST['first_name'];
+    $middleName = $_POST['middle_name'];
+    $lastName = $_POST['last_name'];
+    $contactNo = $_POST['contact_no'];
+    $district = $_POST['district'];
+
+    $query = "INSERT INTO `customer` (title, first_name, middle_name, last_name, contact_no, district) VALUES ('$title', '$firstName', '$middleName', '$lastName', '$contactNo', '$district')";
+
+    $result = mysqli_query($con, $query);
+
+    if ($result) {
+        $submissionStatus = "success";
+    } else {
+        $submissionStatus = "error";
+    }
+}
+
+?>
+
+
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>ERP System</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    </head>
+    <body>
+
+        <form action="customer.php" method="POST">
+    <div class="mb-3 col-md-4">
+        <label for="title" class="form-label">Title</label>
+        <select class="form-select" name="title" id="title">
+            <option value="Mr">Mr</option>
+            <option value="Mrs">Mrs</option>
+            <option value="Miss">Miss</option>
+            <option value="Dr">Dr</option>
+        </select>
+    </div>
+    <div class="row">
+        <div class="mb-1 col-md-6">
+            <label for="first_name" class="form-label">First Name</label>
+            <input type="text" class="form-control" name="first_name" id="first_name" placeholder="First Name" required>
+        </div>
+        <div class="mb-1 col-md-6">
+            <label for="middle_name" class="form-label">Middle Name</label>
+            <input type="text" class="form-control" name="middle_name" id="middle_name" placeholder="Middle Name" required>
+        </div>
+    </div>
+    <div class="mb-1 col-md-12">
+        <label for="last_name" class="form-label">Last Name</label>
+        <input type="text" class="form-control" name="last_name" id="last_name" placeholder="Last Name" required>
+    </div>
+    <div class="mb-1 col-md-12">
+        <label for="contact_no" class="form-label">Contact No.</label>
+        <input type="text" class="form-control" name="contact_no" id="contact_no" placeholder="Contact No." required>
+    </div>
+    <div class="mb-1 col-md-12">
+        <label for="district" class="form-label">District</label>
+        <select class="form-select" name="district" id="district">
+            <?php
+            while ($row = mysqli_fetch_assoc($districtResult)) {
+                $districtId = $row['id'];
+                $districtName = $row['district'];
+                echo "<option value='$districtId'>$districtName</option>";
+            }
+            ?>
+        </select>
+    </div>
+    <button type="submit" class="btn btn-primary col-12" name="submit">Submit</button>
+</form>
+
+
+        <?php if ($submissionStatus === "success"): ?>
+        <script>
+            window.onload = function() {
+                window.opener.location.reload(); // Refresh the parent window to show the updated data
+                window.close(); // Close the form window
+            };
+        </script>
+        <?php endif; ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    </body>
+</html>
