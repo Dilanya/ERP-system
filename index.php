2fd01870f66b17b30d1eda4a18e1615f4dea0165
Dilanya <96423@fhss.sjp.ac.lk>
2023-07-30 04:48:43 +0530

    created the file structure

---
+++ index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ERP System</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid d-flex justify-content-center">
+        <h1 class="text-white " >ERP System</h1>  
+      </div>
+    </nav>
+    <div class="container-fluid" >
+      <div class="row">
+
+        <!-- Sidebar -->
+        <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-dark sidebar">
+          <div class="mt-5">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link text-white" href="?section=customers">Customers</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="?section=items">Items</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="?section=reports">Reports</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <!-- Main Content Area -->
+        <main class="col-md-9 ms-sm-auto col-lg-10 bg-light" style="height: 100vh">
+
+          <?php
+          
+          $defaultSection = isset($_GET['section']) ? $_GET['section'] : 'customers';
+
+          
+          if ($defaultSection === 'customers') {
+            include 'customer.php';
+          } elseif ($defaultSection === 'items') {
+            include 'items.php';
+          } elseif ($defaultSection === 'reports') {
+            include 'reports.php';
+          } else {
+            
+            include 'customer.php';
+            
+          }
+          ?>
+        </main>
+
+      </div>
+    </div>
+  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
+</html>
